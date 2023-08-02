ed29c6686f76ed67f0e1174e191956c3fbddf5c1
Kiran Shila <me@kiranshila.com>
2023-08-01 19:02:37 -0700

    First commit - WIP

---
+++ src/lib.rs
@@ -0,0 +1,135 @@
+//! An implementation of the TFTP Client as specified in [RFC 1350](https://datatracker.ietf.org/doc/html/rfc1350)
+//! This includes retries and timeouts with exponential backoff
+
+use parser::Packet;
+use std::{ffi::CString, io, net::UdpSocket, time::Duration};
+use thiserror::Error;
+use tracing::debug;
+
+pub mod parser;
+
+const BLKSISZE: usize = 512;
+
+struct Context {
+    retries: usize,
+    filename: CString,
+    socket: UdpSocket,
+}
+
+enum State {
+    Send,
+    SendAgain,
+    // Body is the last packet for timeout retries
+    Recv,
+}
+
+pub fn download<T: AsRef<str> + std::fmt::Display>(
+    filename: T,
+    socket: &mut UdpSocket,
+    timeout: Duration,
+    retries: usize,
+) -> Result<Vec<u8>, Error> {
+    // Make sure we can actually timeout, but preserve the old state
+    let old_read_timeout = socket.read_timeout().map_err(Error::SocketIo)?;
+    socket
+        .set_read_timeout(Some(timeout))
+        .map_err(Error::SocketIo)?;
+    debug!("Starting download of {filename}");
+    // Initialize the state of our state machine
+    let mut state = State::Send;
+    let mut local_retries = retries;
+    let mut local_timeout = timeout;
+    let mut send_pkt = Packet::ReadRequest {
+        filename: CString::new(filename.to_string()).map_err(|_| Error::BadFilename)?,
+        mode: parser::RequestMode::Octet,
+    };
+    let mut file_data = vec![];
+    let mut done = false;
+    // Run the state machine
+    loop {
+        match state {
+            State::Send => {
+                local_retries = retries;
+                local_timeout = timeout;
+                let bytes = send_pkt.to_bytes();
+                debug!("Sending bytes - {:?}", bytes);
+                // Send the bytes and reset some other state variables
+                socket.send(&bytes).map_err(Error::SocketIo)?;
+                // Transition to recv if this wasn't the last ACK packet
+                if done {
+                    break;
+                } else {
+                    state = State::Recv
+                }
+            }
+            State::SendAgain => {
+                let bytes = send_pkt.to_bytes();
+                debug!("Retry sending bytes - {:?}", bytes);
+                // Send the bytes and reset some other state variables
+                socket.send(&bytes).map_err(Error::SocketIo)?;
+                // Transition to recv
+                state = State::Recv
+            }
+            State::Recv => {
+                let mut buf = vec![];
+                match socket.recv(&mut buf) {
+                    Ok(_) => (),
+                    Err(e) => {
+                        match e.kind() {
+                            io::ErrorKind::TimedOut => {
+                                // We timed out, try sending the last packet again with exponential backoff
+                                local_retries -= 1;
+                                if local_retries == 0 {
+                                    return Err(Error::Timeout);
+                                }
+                                local_timeout += local_timeout / 2;
+                                socket
+                                    .set_read_timeout(Some(local_timeout))
+                                    .map_err(Error::SocketIo)?;
+                                state = State::SendAgain;
+                                continue;
+                            }
+                            _ => return Err(Error::SocketIo(e)),
+                        }
+                    }
+                }
+                // Process the received packet
+                let recv_pkt = Packet::from_bytes(&buf).map_err(Error::Parse)?;
+                match recv_pkt {
+                    Packet::Data { block_n, data } => {
+                        // We got back a chunk of data, we need to ack it and append to the data we're collecting
+                        file_data.extend_from_slice(&data);
+                        if data.len() < BLKSISZE {
+                            done = true
+                        }
+                        send_pkt = Packet::Acknowledgment { block_n };
+                        state = State::Send;
+                        continue;
+                    }
+                    Packet::Error { code, msg } => todo!(),
+                    _ => return Err(Error::UnexpectedPacket(recv_pkt)),
+                }
+            }
+        }
+    }
+    // Return socket timeout to previous state
+    socket
+        .set_read_timeout(old_read_timeout)
+        .map_err(Error::SocketIo)?;
+    // And return the bytes we downloaded
+    Ok(file_data)
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Bad filename (not a valid CString)")]
+    BadFilename,
+    #[error("Socket IO error - `{0}`")]
+    SocketIo(std::io::Error),
+    #[error("Timeout while trying to complete transaction")]
+    Timeout,
+    #[error("Failed to parse incoming packet")]
+    Parse(parser::Error),
+    #[error("The packet we got back was unexpected")]
+    UnexpectedPacket(Packet),
+}
---
+++ src/parser.rs
@@ -0,0 +1,265 @@
+//! Parser and serialization of the TFTP [`Packet`]
+
+use byte_strings::c_str;
+use std::ffi::{CStr, CString};
+use thiserror::Error;
+
+#[repr(u16)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum ErrorCode {
+    Unspec = 0,
+    NoFile = 1,
+    Access = 2,
+    Write = 3,
+    Op = 4,
+    BadId = 5,
+    Exist = 6,
+    BadUser = 7,
+    BadOpt = 8,
+}
+
+impl ErrorCode {
+    fn from_u16(v: u16) -> Result<Self, Error> {
+        Ok(match v {
+            0 => ErrorCode::Unspec,
+            1 => ErrorCode::NoFile,
+            2 => ErrorCode::Access,
+            3 => ErrorCode::Write,
+            4 => ErrorCode::Op,
+            5 => ErrorCode::BadId,
+            6 => ErrorCode::Exist,
+            7 => ErrorCode::BadUser,
+            8 => ErrorCode::BadOpt,
+            _ => return Err(Error::BadErrorCode(v)),
+        })
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum RequestMode {
+    Octet,
+    NetAscii,
+    Mail,
+}
+
+impl RequestMode {
+    fn from_cstr(str: &CStr) -> Result<Self, Error> {
+        Ok(
+            match str
+                .to_str()
+                .map_err(|_| Error::BadString)?
+                .to_ascii_lowercase()
+                .as_str()
+            {
+                "octet" => Self::Octet,
+                "netascii" => Self::NetAscii,
+                "mail" => Self::Mail,
+                _ => return Err(Error::BadString),
+            },
+        )
+    }
+
+    fn into_cstr(self) -> &'static CStr {
+        match self {
+            RequestMode::Octet => c_str!("octet"),
+            RequestMode::NetAscii => c_str!("netascii"),
+            RequestMode::Mail => c_str!("mail"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Packet {
+    ReadRequest {
+        filename: CString,
+        mode: RequestMode,
+    },
+    WriteRequest {
+        filename: CString,
+        mode: RequestMode,
+    },
+    Data {
+        block_n: u16,
+        data: Vec<u8>,
+    },
+    Acknowledgment {
+        block_n: u16,
+    },
+    Error {
+        code: ErrorCode,
+        msg: CString,
+    },
+}
+
+impl Packet {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = vec![];
+        match self {
+            Packet::ReadRequest { filename, mode } => {
+                buf.extend_from_slice(&1u16.to_ne_bytes());
+                buf.extend_from_slice(filename.to_bytes_with_nul());
+                buf.extend_from_slice(mode.into_cstr().to_bytes_with_nul());
+            }
+            Packet::WriteRequest { filename, mode } => {
+                buf.extend_from_slice(&2u16.to_ne_bytes());
+                buf.extend_from_slice(filename.to_bytes_with_nul());
+                buf.extend_from_slice(mode.into_cstr().to_bytes_with_nul());
+            }
+            Packet::Data { block_n, data } => {
+                buf.extend_from_slice(&3u16.to_ne_bytes());
+                buf.extend_from_slice(&block_n.to_ne_bytes());
+                buf.extend_from_slice(data);
+            }
+            Packet::Acknowledgment { block_n } => {
+                buf.extend_from_slice(&4u16.to_ne_bytes());
+                buf.extend_from_slice(&block_n.to_ne_bytes());
+            }
+            Packet::Error { code, msg } => {
+                buf.extend_from_slice(&5u16.to_ne_bytes());
+                buf.extend_from_slice(&(*code as u16).to_ne_bytes());
+                buf.extend_from_slice(msg.as_bytes_with_nul());
+            }
+        }
+        buf
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        if bytes.len() < 4 {
+            // Check against the smallest payload size (ACK)
+            return Err(Error::Incomplete);
+        }
+        // Now we're guaranteed to at least have the opcode
+        let opcode = u16::from_ne_bytes(bytes[0..2].try_into().unwrap());
+        let body = &bytes[2..];
+        match opcode {
+            // RRQ
+            1 => {
+                // Smallest size after the opcode is 7 bytes
+                // 2 bytes for 1 char filename and 5 bytes for "mail" mode
+                if body.len() < 7 {
+                    Err(Error::Incomplete)
+                } else {
+                    // The rest should have exactly two null bytes, one for each string
+                    let mut iter = body.splitn(3, |x| *x == 0);
+                    let filename = iter.next().ok_or(Error::Incomplete)?;
+                    let mode = iter.next().ok_or(Error::Incomplete)?;
+                    Ok(Packet::ReadRequest {
+                        filename: CString::new(filename).map_err(|_| Error::BadString)?,
+                        mode: RequestMode::from_cstr(
+                            &CString::new(mode).map_err(|_| Error::BadString)?,
+                        )?,
+                    })
+                }
+            }
+            // WRQ
+            2 => {
+                // Same story as RRQ, but different discriminant
+                if body.len() < 7 {
+                    Err(Error::Incomplete)
+                } else {
+                    // The rest should have exactly two null bytes, one for each string
+                    let mut iter = body.splitn(3, |x| *x == 0);
+                    let filename = iter.next().ok_or(Error::Incomplete)?;
+                    let mode = iter.next().ok_or(Error::Incomplete)?;
+                    Ok(Packet::WriteRequest {
+                        filename: CString::new(filename).map_err(|_| Error::BadString)?,
+                        mode: RequestMode::from_cstr(
+                            &CString::new(mode).map_err(|_| Error::BadString)?,
+                        )?,
+                    })
+                }
+            }
+            // DATA
+            3 => {
+                // Minimum data body size is a block num of 2 bytes and 0 data bytes,
+                if body.len() < 2 {
+                    Err(Error::Incomplete)
+                } else {
+                    let block_n = u16::from_ne_bytes(body[..2].try_into().unwrap());
+                    let data = body[2..].to_vec();
+                    Ok(Packet::Data { block_n, data })
+                }
+            }
+            // ACK
+            4 => {
+                // We've already checked length for this smallest payload
+                let block_n = u16::from_ne_bytes(body[..2].try_into().unwrap());
+                Ok(Packet::Acknowledgment { block_n })
+            }
+            // ERROR
+            5 => {
+                // Minimum size here is 3 bytes, 2 for the error code and 1 for a zero length string (null byte)
+                if body.len() < 3 {
+                    Err(Error::Incomplete)
+                } else {
+                    let code =
+                        ErrorCode::from_u16(u16::from_ne_bytes(body[0..2].try_into().unwrap()))?;
+                    // The rest should have exactly one null byte at the end for the string
+                    if *body[2..].last().unwrap() != 0 {
+                        Err(Error::BadString)
+                    } else {
+                        let msg = CString::new(&body[2..(body.len() - 1)])
+                            .map_err(|_| Error::BadString)?;
+                        Ok(Packet::Error { code, msg })
+                    }
+                }
+            }
+            _ => Err(Error::BadOpcode(opcode)),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Too few bytes recieved")]
+    Incomplete,
+    #[error("Opcode wasn't expected - `{0}`")]
+    BadOpcode(u16),
+    #[error("String in payload wasn't a valid CString or was otherwise invalid")]
+    BadString,
+    #[error("Error code wasn't recognized - `{0}`")]
+    BadErrorCode(u16),
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use paste::paste;
+
+    macro_rules! test_happy_packet {
+        ($packet:expr, $name:literal) => {
+            paste! {
+                #[test]
+                fn [<test_ $name>]() {
+                    let pkt = $packet;
+                    // Serialize to bytes
+                    let bytes = pkt.to_bytes();
+                    // And back to packet
+                    let pkt_parsed = Packet::from_bytes(&bytes).unwrap();
+                    // And check
+                    assert_eq!(pkt, pkt_parsed);
+                }
+            }
+        };
+    }
+
+    test_happy_packet! {Packet::ReadRequest {filename:CString::new("foo").unwrap(), mode: RequestMode::Octet}, "rrq_octet"}
+    test_happy_packet! {Packet::ReadRequest {filename:CString::new("foo").unwrap(), mode: RequestMode::NetAscii}, "rrq_netascii"}
+    test_happy_packet! {Packet::ReadRequest {filename:CString::new("foo").unwrap(), mode: RequestMode:: Mail}, "rrq_mail"}
+    test_happy_packet! {Packet::WriteRequest {filename:CString::new("foo").unwrap(), mode: RequestMode::Octet}, "wrq_octet"}
+    test_happy_packet! {Packet::WriteRequest {filename:CString::new("foo").unwrap(), mode: RequestMode::NetAscii}, "wrq_netascii"}
+    test_happy_packet! {Packet::WriteRequest {filename:CString::new("foo").unwrap(), mode: RequestMode:: Mail}, "wrq_mail"}
+    test_happy_packet! {Packet::Data {block_n: 42, data: vec![0xDE, 0xAD, 0xBE, 0xEF]}, "data"}
+    test_happy_packet! {Packet::Data {block_n: 123, data: vec![]}, "data_empty"}
+    test_happy_packet! {Packet::Acknowledgment { block_n: 42 }, "ack"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::Unspec, msg: CString::new("Msg").unwrap() }, "error_unspec"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::NoFile, msg: CString::new("Msg").unwrap() }, "error_nofile"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::Access, msg: CString::new("Msg").unwrap() }, "error_access"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::Write, msg: CString::new("Msg").unwrap() }, "error_write"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::Op, msg: CString::new("Msg").unwrap() }, "error_op"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::BadId, msg: CString::new("Msg").unwrap() }, "error_badid"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::Exist, msg: CString::new("Msg").unwrap() }, "error_exist"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::BadUser, msg: CString::new("Msg").unwrap() }, "error_baduser"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::BadOpt, msg: CString::new("Msg").unwrap() }, "error_badopt"}
+    test_happy_packet! {Packet::Error { code: ErrorCode::BadOpt, msg: CString::new("").unwrap() }, "error_empty"}
+}
