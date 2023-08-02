c86ab3f832d4284194f2eabb1750f5dfc312b630
Kiran Shila <me@kiranshila.com>
2023-08-01 20:02:19 -0700

    Bind and connect example

--- examples/download.rs
+++ examples/download.rs
@@ -1,9 +1,13 @@
-use std::{net::UdpSocket, time::Duration};
+use std::{
+    net::{SocketAddr, UdpSocket},
+    time::Duration,
+};
 
 use tftp_client::download;
 
 fn main() -> std::io::Result<()> {
-    let mut socket = UdpSocket::bind("192.168.0.3:69")?;
+    let mut socket = UdpSocket::bind("127.0.0.1:0")?;
+    socket.connect("192.168.0.3:69")?;
 
     let timeout = Duration::from_millis(100);
     let max_timeout = Duration::from_secs(5);
