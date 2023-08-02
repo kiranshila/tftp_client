ad7cf881aa731244577e2dab93764af99b88d916
Kiran Shila <me@kiranshila.com>
2023-08-01 20:04:41 -0700

    Correct bind

--- examples/download.rs
+++ examples/download.rs
@@ -6,7 +6,7 @@ use std::{
 use tftp_client::download;
 
 fn main() {
-    let mut socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let mut socket = UdpSocket::bind("0.0.0.0:0").unwrap();
     socket.connect("192.168.0.3:69").unwrap();
 
     let timeout = Duration::from_millis(100);
