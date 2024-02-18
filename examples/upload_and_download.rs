e690b06a2217dca72070b1f9b609f2fb0238b9e8
Kiran Shila <me@kiranshila.com>
2024-02-17 17:10:08 -0800

    Add new example

---
+++ examples/upload_and_download.rs
@@ -0,0 +1,28 @@
+use std::{net::UdpSocket, time::Duration};
+
+use tftp_client::{download, upload};
+
+fn main() {
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let server = "127.0.0.1:69".parse().unwrap();
+
+    let timeout = Duration::from_millis(100);
+    let max_timeout = Duration::from_secs(5);
+    let retries = 8;
+
+    let test_payload = vec![0xb0, 0xba, 0xca, 0xfe];
+    upload(
+        "/test",
+        &test_payload,
+        &socket,
+        server,
+        timeout,
+        max_timeout,
+        retries,
+    )
+    .unwrap();
+
+    let res = download("/test", &socket, server, timeout, max_timeout, retries).unwrap();
+
+    assert_eq!(test_payload, res);
+}
