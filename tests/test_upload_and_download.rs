8eae6383cabfc07752bd7f2eb4d57019001757b1
Kiran Shila <me@kiranshila.com>
2024-02-17 17:42:13 -0800

    Make the test a test

--- tests/test_upload_and_download.rs
+++ tests/test_upload_and_download.rs
@@ -1,21 +1,13 @@
-use std::{
-    net::UdpSocket,
-    time::Duration,
-};
+use std::{net::UdpSocket, time::Duration};
+use tftp_client::{download, upload};
 
-use tftp_client::{
-    download,
-    upload,
-};
-
-fn main() {
+#[test]
+fn download_upload() {
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
     let server = "127.0.0.1:69".parse().unwrap();
-
     let timeout = Duration::from_millis(100);
     let max_timeout = Duration::from_secs(5);
     let retries = 8;
-
     let test_payload = vec![0xb0, 0xba, 0xca, 0xfe];
     upload(
         "/test",
@@ -27,8 +19,6 @@ fn main() {
         retries,
     )
     .unwrap();
-
     let res = download("/test", &socket, server, timeout, max_timeout, retries).unwrap();
-
     assert_eq!(test_payload, res);
 }
